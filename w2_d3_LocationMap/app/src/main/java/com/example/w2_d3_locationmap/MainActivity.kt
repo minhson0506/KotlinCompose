b2af2b457ebac1139d36deef32de6d0308773e98
Son Dang <minhson0506@gmail.com>
2022-09-07 19:37:03 +0300

    lab location and map

--- w2_d3_LocationMap/app/src/main/java/com/example/w2_d3_locationmap/MainActivity.kt
+++ w2_d3_LocationMap/app/src/main/java/com/example/w2_d3_locationmap/MainActivity.kt
@@ -4,11 +4,12 @@ import android.Manifest
 import android.app.Activity
 import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.Point
+import android.location.Geocoder
 import android.location.Location
 import android.os.Build
 import android.os.Bundle
 import android.os.Looper
-import android.os.ProxyFileDescriptorCallback
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -18,29 +19,98 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.preference.PreferenceManager
 import com.example.w2_d3_locationmap.ui.theme.W2_d3_LocationMapTheme
 import com.google.android.gms.location.*
 import com.google.android.gms.location.Priority.PRIORITY_HIGH_ACCURACY
-import java.math.BigDecimal
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.Marker
+import java.util.*
 import kotlin.math.round
+
 
 class MainActivity : ComponentActivity() {
     private val TAG = "w2d3LocationMap"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         checkPermission(this, this)
+        val context = this.applicationContext
+        val activity = this@MainActivity
+
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
         setContent {
             W2_d3_LocationMapTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    getLocation(this, this)
+                    Column() {
+                        getLocation(context = context, activity = activity)
+                    }
                 }
             }
         }
     }
+}
+
+@Composable
+fun composeMap(): MapView {
+    val context = LocalContext.current
+    val mapView = remember {
+        MapView(context).apply { id = R.id.map }
+    }
+    return mapView
+}
+
+@Composable
+fun showMap(geoPoint: GeoPoint, address: String) {
+    val map = composeMap()
+    var mapInitialized by remember(map) { mutableStateOf(false) }
+    if (!mapInitialized) {
+        map.setTileSource(TileSourceFactory.MAPNIK)
+        map.controller.setZoom(17.0)
+        map.controller.setCenter(GeoPoint(60.17, 24.95))
+        mapInitialized = true
+    }
+
+    val marker = Marker(map)
+    AndroidView(factory = { map }) {
+        address ?: return@AndroidView
+        it.controller.setCenter(geoPoint)
+        marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+        marker.position = geoPoint
+        marker.showInfoWindow()
+        marker.title = address + "; lat ${geoPoint.latitude} & long ${geoPoint.longitude}"
+        
+        map.overlays.filter {  }
+        map.overlays.clear()
+        map.overlays.add(marker)
+        map.invalidate()
+    }
+}
+
+fun getAddress(context: Context, lat: Double, long: Double): String {
+    var TAG = "w2_d3_LocationMap"
+    val geocoder = Geocoder(context, Locale.getDefault())
+    var address = ""
+    try {
+        address = if (Build.VERSION.SDK_INT >= 33) {
+            val location = geocoder.getFromLocation(lat, long, 1)
+            location.first().getAddressLine(0)
+        } else {
+            geocoder.getFromLocation(lat, long, 1)?.first()?.getAddressLine(0) ?: ""
+        }
+    } catch (e: Error) {
+        Log.d(TAG, "getAddress: not response")
+    }
+    return address
 }
 
 @Composable
@@ -52,8 +122,14 @@ fun getLocation(context: Context, activity: Activity) {
     var speed by remember {
         mutableStateOf(0.0)
     }
+    var maxSpeed = 0.0
     var preLocation: Location? = null
-
+    var long by remember {
+        mutableStateOf(0.0)
+    }
+    var lat by remember {
+        mutableStateOf(0.0)
+    }
 
     var fusedLocationCLient: FusedLocationProviderClient =
         LocationServices.getFusedLocationProviderClient(activity)
@@ -73,20 +149,28 @@ fun getLocation(context: Context, activity: Activity) {
             super.onLocationResult(locationResult)
             locationResult ?: return
             for (location in locationResult.locations) {
-                if (preLocation != null) distance = location.distanceTo(preLocation).toDouble()
+                if (preLocation != null) {
+                    distance = round(location.distanceTo(preLocation)).toDouble()
+                } else {
+                    preLocation = location
+                }
                 speed = round(location.speed).toDouble()
+                if (speed > maxSpeed) maxSpeed = speed
                 Log.d(TAG, "distance is: $distance")
                 Log.d(TAG, "speed is: $speed")
                 Log.d(TAG,
                     "location latitude: ${location.latitude} and longitude ${location.longitude}")
-                preLocation = location
+                //preLocation = location
+                long = location.longitude
+                lat = location.latitude
             }
         }
     }
 
     Column() {
-        Text(text = "Distance is: $distance")
-        Text(text = "Speed is: $speed")
+        Text(text = "Distance is: $distance m")
+        Text(text = "Speed is: $speed m/s")
+        Text(text = "Fastest speed is: $maxSpeed m/s")
         Button(onClick = {
             val locationRequest = LocationRequest.create().setInterval(1000)
                 .setPriority(PRIORITY_HIGH_ACCURACY)
@@ -101,6 +185,8 @@ fun getLocation(context: Context, activity: Activity) {
         }) {
             Text(text = "Stop getting Location")
         }
+        if (lat != 0.0 && long != 0.0) showMap(geoPoint = GeoPoint(lat, long),
+            address = getAddress(context = context, lat, long))
     }
 }
 
